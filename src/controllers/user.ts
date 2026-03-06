bccbef8ee8bc2e0f1dda67226933bc17890d7373
Maciej Zboch <maciejzboch@gmail.com>
2026-03-06 22:09:35 +0100

    fix: change file name to fit naming convention

---
+++ src/controllers/user.ts
@@ -0,0 +1,53 @@
+import { Request, Response } from "express";
+import User from "../models/user";
+import RefreshToken from "../models/refreshToken";
+import { hashPassword, comparePassword } from "../utils/hash";
+import { generateAccessToken, generateRefreshToken } from "../utils/jwt";
+
+export const register = async (req: Request, res: Response) => {
+  const { username, email, password } = req.body;
+
+  const hashed = await hashPassword(password);
+
+  const user = await User.create({
+    username,
+    email,
+    password: hashed,
+  });
+
+  res.status(201).json({ message: "User created", userId: user._id });
+};
+
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) return res.status(401).json({ message: "Invalid credentials" });
+
+  const valid = await comparePassword(password, user.password);
+
+  if (!valid) return res.status(401).json({ message: "Invalid credentials" });
+
+  const accessToken = generateAccessToken(user._id.toString());
+  const refreshToken = generateRefreshToken(user._id.toString());
+
+  await RefreshToken.create({
+    userId: user._id,
+    token: refreshToken,
+    expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+  });
+
+  res.json({
+    accessToken,
+    refreshToken,
+  });
+};
+
+export const logout = async (req: Request, res: Response) => {
+  const { refreshToken } = req.body;
+
+  await RefreshToken.deleteOne({ token: refreshToken });
+
+  res.json({ message: "Logged out" });
+};
