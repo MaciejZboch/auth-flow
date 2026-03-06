d5f2caeff501e2528de1c4c4441013908bae1e4c
Maciej Zboch <maciejzboch@gmail.com>
2026-03-06 22:15:09 +0100

    feat: add login and logout routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,11 +1,13 @@
 import express from "express";
-import { register } from "../controllers/user";
+import { register, login, logout } from "../controllers/user";
 import catchAsync from "../middleware/catchAsync";
 import { validate } from "../middleware/validate";
-import { registerSchema } from "../validators/userSchemas";
+import { loginSchema, registerSchema } from "../validators/userSchemas";
 const router = express.Router();
 
 //router.get("/", index);
 router.post("/register", validate(registerSchema), catchAsync(register));
+router.post("/login", validate(loginSchema), catchAsync(login));
+router.post("/logout", catchAsync(logout));
 
 export default router;
