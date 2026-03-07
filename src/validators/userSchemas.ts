c0b0307701a2a1ad530bd384e524228e407415d8
Maciej Zboch <maciejzboch@gmail.com>
2026-03-07 12:11:00 +0100

    fix: tweak JOI validation

--- src/validators/userSchemas.ts
+++ src/validators/userSchemas.ts
@@ -1,16 +1,12 @@
 import Joi from "joi";
 
 export const registerSchema = Joi.object({
-  user: Joi.object({
-    username: Joi.string().required().min(3).max(20),
-    password: Joi.string().required().min(6).max(20),
-    email: Joi.string().required(),
-  }).required(),
+  username: Joi.string().required().min(3).max(20),
+  password: Joi.string().required().min(6).max(20),
+  email: Joi.string().required(),
 });
 
 export const loginSchema = Joi.object({
-  user: Joi.object({
-    username: Joi.string().required().min(3).max(20),
-    password: Joi.string().required().min(6).max(20),
-  }).required(),
+  username: Joi.string().required().min(3).max(20),
+  password: Joi.string().required().min(6).max(20),
 });
