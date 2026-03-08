ad717f67729ae3a651b6c73db136a4e68c48cd47
Maciej Zboch <maciejzboch@gmail.com>
2026-03-08 17:45:42 +0100

    fix: add more validations

--- src/validators/userSchemas.ts
+++ src/validators/userSchemas.ts
@@ -1,17 +1,27 @@
 import Joi from "joi";
 
 export const registerSchema = Joi.object({
-  username: Joi.string().required().min(3).max(20),
-  password: Joi.string().required().min(6).max(20),
-  email: Joi.string().required(),
-});
+  username: Joi.string()
+    .trim()
+    .pattern(/^[a-zA-Z0-9_-]+$/)
+    .required()
+    .min(3)
+    .max(20),
+  password: Joi.string().trim().required().min(6).max(20),
+  email: Joi.string().trim().email().lowercase().required(),
+}).unknown(false);
 
 export const loginSchema = Joi.object({
-  email: Joi.string().required(),
-  password: Joi.string().required().min(6).max(20),
-});
+  email: Joi.string().trim().email().lowercase().required(),
+  password: Joi.string().trim().required().min(6).max(20),
+}).unknown(false);
 
 export const updateSchema = Joi.object({
-  email: Joi.string().required(),
-  username: Joi.string().required().min(6).max(20),
-});
+  email: Joi.string().trim().email().lowercase().required(),
+  username: Joi.string()
+    .trim()
+    .pattern(/^[a-zA-Z0-9_-]+$/)
+    .required()
+    .min(3)
+    .max(20),
+}).unknown(false);
